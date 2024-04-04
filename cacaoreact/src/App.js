4c00e57582adec8f1f422600bae280094fbc4d96
Aixopluc <alextopee@gmail.com>
2024-04-04 15:10:33 +0200

    GO API 1 version

--- cacaoreact/src/App.js
+++ cacaoreact/src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  // Estado para almacenar los datos del formulario
+  const [formulario, setFormulario] = useState({
+    NumeroDePale: '',
+    Kg: '',
+    Lote: '',
+    Ubicacion: '',
+    Estado: '',
+    Expedido: false,
+    Producto: ''
+  });
+
+  // Función para manejar el cambio en los inputs del formulario
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormulario({
+      ...formulario,
+      [name]: value
+    });
+  }
+
+  // Función para enviar los datos del formulario al backend
+  const enviarDatosFormulario = async () => {
+    try {
+      // Convertir campos relevantes a los tipos de datos correctos
+      const datosFormulario = {
+        ...formulario,
+        Kg: parseFloat(formulario.Kg),
+        NumeroDePale: parseInt(formulario.NumeroDePale)
+      };
+  
+      // Enviar solicitud POST con los datos actualizados
+      const respuesta = await axios.post('http://localhost:8080/pales', datosFormulario);
+      console.log(respuesta.data); // Puedes mostrar un mensaje de éxito o hacer algo más con la respuesta
+    } catch (error) {
+      console.error('Error al enviar los datos:', error);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Formulario para enviar datos al backend en Go:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" name="Producto" value={formulario.Producto} onChange={handleInputChange} placeholder="Producto" />
+        <input type="number" name="Kg" value={formulario.Kg} onChange={handleInputChange} placeholder="Kg" />
+        <input type="text" name="Lote" value={formulario.Lote} onChange={handleInputChange} placeholder="Lote" />
+        <input type="text" name="Ubicacion" value={formulario.Ubicacion} onChange={handleInputChange} placeholder="Ubicación" />
+        <input type="text" name="Estado" value={formulario.Estado} onChange={handleInputChange} placeholder="Estado" />
+        <input type="text" name="NumeroDePale" value={formulario.NumeroDePale} onChange={handleInputChange} placeholder="Número de Pale" />
+        <button onClick={enviarDatosFormulario}>Enviar datos</button>
       </header>
     </div>
   );
---
+++ controllers/paleControllers.go
@@ -0,0 +1,101 @@
+package controllers
+
+import (
+	"cacaodelight/initializers"
+	"cacaodelight/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func PaleCreate(c *gin.Context) {
+	// Analizar el JSON de la solicitud en una estructura Pale
+	var paleData models.Pale
+	if err := c.BindJSON(&paleData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Crear un nuevo registro Pale con los datos proporcionados
+	result := initializers.DB.Create(&paleData)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al crear el registro Pale"})
+		return
+	}
+
+	// Respuesta exitosa con el registro creado
+	c.JSON(http.StatusCreated, gin.H{"pale": paleData})
+}
+
+func GetAllPales(c *gin.Context) {
+	var pales []models.Pale
+	initializers.DB.Find(&pales)
+
+	c.JSON(200, gin.H{
+		"pales": pales,
+	})
+
+}
+
+func GetPaleById(c *gin.Context) {
+
+	id := c.Param("ID")
+	var pales models.Pale
+	initializers.DB.First(&pales, id)
+
+	c.JSON(200, gin.H{
+		"pales": pales,
+	})
+
+}
+
+func PaleUpdate(c *gin.Context) {
+	// Paso 1: Parsear el ID del parámetro de la URL
+	id := c.Param("ID")
+
+	// Paso 2: Vincular los datos del JSON de la solicitud a una estructura en Go
+	var paleUpdateData models.Pale
+	if err := c.BindJSON(&paleUpdateData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Paso 3: Recuperar el registro de la base de datos que deseas actualizar utilizando GORM
+	var pale models.Pale
+	if err := initializers.DB.First(&pale, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Registro no encontrado"})
+		return
+	}
+
+	// Paso 4: Actualizar los campos del registro con los datos proporcionados en la solicitud
+	// Actualiza solo los campos que están presentes en la solicitud
+	if paleUpdateData.NumeroDePale != 0 {
+		pale.NumeroDePale = paleUpdateData.NumeroDePale
+	}
+	if paleUpdateData.Kg != 0 {
+		pale.Kg = paleUpdateData.Kg
+	}
+	if paleUpdateData.Lote != "" {
+		pale.Lote = paleUpdateData.Lote
+	}
+	if paleUpdateData.Ubicacion != "" {
+		pale.Ubicacion = paleUpdateData.Ubicacion
+	}
+	if paleUpdateData.Estado != "" {
+		pale.Estado = paleUpdateData.Estado
+	}
+	if paleUpdateData.Expedido {
+		pale.Expedido = paleUpdateData.Expedido
+	}
+	if paleUpdateData.Producto != "" {
+		pale.Producto = paleUpdateData.Producto
+	}
+	// Paso 5: Guardar los cambios en la base de datos
+	if err := initializers.DB.Save(&pale).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al guardar en la base de datos"})
+		return
+	}
+
+	// Respuesta exitosa
+	c.JSON(http.StatusOK, pale)
+}
